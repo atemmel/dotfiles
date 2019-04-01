e448e6c9a789d7f1f7cbbc0fc53a7384ad991de3
atemmel <temmeladam@gmail.com>
2019-04-01 06:51:36 +0200

    Changed bars and border

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,9 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
 #polybar example -r &
-polybar example &
-#polybar bar2 &
-
-echo "Bars launched..."
+polybar main &
+polybar lower &
