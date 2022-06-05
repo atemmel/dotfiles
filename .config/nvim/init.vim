a2b0f49f75cdec0bd06d312f4b5cca74c0892039
atemmel <temmeladam@gmail.com>
2022-06-05 17:11:00 +0200

    Updated nvim config

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,3 +1,48 @@
-set runtimepath^=/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-source ~/.vimrc
+filetype off
+set rtp+=~/.config/nvim/bundle/Vundle.vim
+
+call vundle#begin()
+
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'dylanaraps/wal.vim'
+Plugin 'neovim/nvim-lspconfig'
+Plugin 'hrsh7th/nvim-cmp'
+Plugin 'hrsh7th/cmp-nvim-lsp'
+Plugin 'hrsh7th/cmp-buffer'
+Plugin 'hrsh7th/cmp-cmdline'
+Plugin 'hrsh7th/cmp-path'
+Plugin 'L3MON4D3/LuaSnip'
+
+call vundle#end()
+
+colorscheme wal
+
+set number
+set scrolloff=7
+set ts=4 sw=4
+highlight Comment cterm=italic gui=italic
+
+filetype plugin indent on
+
+nnoremap Q <nop>
+nnoremap <SPACE> <nop>
+
+nnoremap j gj
+nnoremap k gk
+nnoremap J o
+nnoremap K O
+nnoremap q :q <CR>
+nnoremap W :w <CR>
+nnoremap E :find<SPACE>
+nnoremap <C-e> :e<SPACE>
+nnoremap e w
+vnoremap e w
+nnoremap w b
+vnoremap w b
+nnoremap U <C-r>
+nnoremap <C-h> {
+nnoremap <C-l> }
+
+vmap <S-c> "+y
+
+lua require'temmel'
---
+++ .config/nvim/lua/temmel.lua
@@ -0,0 +1,62 @@
+-- For debugging
+-- vim.lsp.set_log_level("debug")
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+require'lspconfig'.zls.setup{}
+require'lspconfig'.gopls.setup{
+	capabilities = capabilities,
+	on_attach = function()
+		vim.keymap.set("n", "H", vim.lsp.buf.hover, {buffer=0})
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+		vim.keymap.set("n", "cn", vim.lsp.buf.rename, {buffer=0})
+	end,
+}
+
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local cmp = require'cmp'
+local luasnip = require'luasnip'
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = {
+		['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+		['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	},
+    sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+		{ name = 'buffer' },
+	})
+})
