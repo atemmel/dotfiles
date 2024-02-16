894c65953829ce5020046bb40506dc11378f5686
Adam Temmel <adam.temmel@bolagsverket.se>
2024-02-16 20:58:18 +0100

    webstuff

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,3 +1,5 @@
+local utils = require "utils"
+
 local au = vim.api.nvim_create_autocmd;
 
 au(
@@ -12,15 +14,55 @@ if vim.fn.exists("current_compiler") ~= 0 then
 	vim.api.nvim_command("set makeprg=make -C build")
 end
 
+local prettier_files = {
+	"css",
+	"html",
+	"javascript",
+	"json",
+	"scss",
+	"typescript",
+	"vue",
+}
+
+local prettier = require("prettier")
+
+local function format_xml()
+	local contents = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+	local cmd = string.format("echo '%s' | xmllint --format -", table.concat(contents, "\n"))
+	local file = io.popen(cmd, "r")
+	if file == nil then
+		return
+	end
+	local output = file:read("*a")
+	local list = utils.split(output, "\n")
+	vim.api.nvim_buf_set_lines(0, 0, -1, false, list)
+end
+
+function Format()
+	for _, value in pairs(prettier_files) do
+		if value == vim.bo.filetype then
+			prettier.format()
+			return
+		end
+	end
+	if vim.bo.filetype == "xml" then
+		format_xml()
+	else
+		vim.lsp.buf.format()
+	end
+end
+
 au(
 	"Filetype",
 	{
 		pattern = {
+			"*.css",
+			"*.html",
 			"*.js",
+			"*.json",
+			"*.scss",
 			"*.ts",
 			"*.vue",
-			"*.html",
-			"*.json",
 		},
 		command = "setlocal expandtab tabstop=2, shiftwidth=2, softtabstop=2",
 	}
@@ -30,6 +72,6 @@ au(
 	"BufWritePre",
 	{
 		pattern = "*",
-		command = "lua vim.lsp.buf.format()",
+		command = "lua Format()",
 	}
 )
