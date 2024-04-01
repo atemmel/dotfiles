4147f967367f15c35c317932647a16096e92f068
atemmel <temmeladam@gmail.com>
2024-04-01 20:47:54 +0200

    new nvim package manager

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,25 @@
+local rocks_config = {
+	rocks_path = "/home/temmel/.local/share/nvim/rocks",
+	luarocks_binary = "/home/temmel/.local/share/nvim/rocks/bin/luarocks",
+}
+
+vim.g.rocks_nvim = rocks_config
+
+local luarocks_path = {
+	vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
+	vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
+}
+package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
+
+local luarocks_cpath = {
+	vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
+	vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
+}
+package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
+
+vim.opt.runtimepath:append(vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "*", "*"))
+
+--[[
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	print("Installing Lazy...")
@@ -32,6 +54,7 @@ require "lazy".setup({
 	"nvim-telescope/telescope.nvim",
 	"nvim-treesitter/nvim-treesitter",
 })
+--]]
 
 require "neodev".setup({})
 
@@ -48,5 +71,6 @@ end
 require "lsp"
 require "statusline"
 require "autocmd"
+require "diagrams"
 Utils = require "utils"
 Projects = require "projects"
