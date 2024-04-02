f6d3c94b161ac0427e673e939de67b280074f6aa
atemmel <temmeladam@gmail.com>
2024-04-02 23:01:33 +0200

    fixed monitor config

--- bin/monitors.sh
+++ bin/monitors.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
-targetresolution="1920x1080"
-#targetresolution="1280x720"
-#targetresolution="1600x900"
-#targetresolution="2560x1440"
-#targetresolution="3440x1440"
 
 choice=$(echo -e "left\nright\ntop\nmirror\nrestore" | rofi -dmenu -i -p "Use external monitor as")
 [ -z "$choice" ] && exit 0
 
+xrandr --output eDP-1 primary
+
+line=$(xrandr | grep ' connected' | grep -v 'eDP-1')
+targetmonitor=$(echo $line | cut -d " " -f1)
+targetresolution=$(echo $line | sed -r 's/.* ([0-9]+x[0-9]+).*/\1/')
+
+xrandr --output eDP-1 --transform none
+
 case $choice in
-	left) xrandr --output DP-1 --mode "$targetresolution" --left-of eDP-1;;
-	right) xrandr --output DP-1 --mode "$targetresolution" --right-of eDP-1;;
-	top) xrandr --output DP-1 --mode "$targetresolution" --above eDP-1;;
+	left) xrandr --output $targetmonitor --mode "$targetresolution" --left-of eDP-1;;
+	right) xrandr --output $targetmonitor --mode "$targetresolution" --right-of eDP-1;;
+	top) xrandr --output $targetmonitor --mode "$targetresolution" --above eDP-1;;
 	restore) xrandr --output eDP-1 --transform none;;
-	mirror) xrandr --fb "$targetresolution" --output eDP-1 --mode 1600x900 --scale-from "$targetresolution" --output HDMI-1 --mode "$targetresolution" --scale 1x1 --same-as eDP-1
+	mirror) xrandr --fb "$targetresolution" --output eDP-1 --mode 1600x900 --scale-from "$targetresolution" --output $targetmonitor --mode "$targetresolution" --scale 1x1 --same-as eDP-1
 ;;
 esac
