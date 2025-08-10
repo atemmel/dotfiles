ba7d546356811461df65522d5a39c1a34c15183d
atemmel <temmeladam@gmail.com>
2025-08-10 23:35:58 +0200

    suck

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -76,26 +76,46 @@ require "lspconfig".jdtls.setup {
     },
 }
 
-require "lspconfig".volar.setup {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    filetypes = {
-        "javascript",
-        "javascript-react",
-        "json",
-        "typescript",
-        "typescript-react",
-        "vue",
-    },
-    init_options = {
-        typescript = {
-            tsdk = "/usr/lib/node_modules/typescript/lib",
-        },
-        vue = {
-            hybridMode = false,
-        },
-    },
+local vue_language_server_path = vim.fn.stdpath('data') ..
+    "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+
+local vue_plugin = {
+    name = '@vue/typescript-plugin',
+    location = vue_language_server_path,
+    languages = { 'vue' },
+    configNamespace = 'typescript',
 }
+
+local vtsls_config = {
+    settings = {
+        vtsls = {
+            tsserver = {
+                globalPlugins = {
+                    vue_plugin,
+                },
+            },
+        },
+    },
+    filetypes = tsserver_filetypes,
+}
+
+
+local ts_ls_config = {
+    init_options = {
+        plugins = {
+            vue_plugin,
+        },
+    },
+    filetypes = tsserver_filetypes,
+}
+
+local vue_ls_config = {}
+
+vim.lsp.config('vtsls', vtsls_config)
+vim.lsp.config('vue_ls', vue_ls_config)
+vim.lsp.config('ts_ls', ts_ls_config)
+vim.lsp.enable({ 'ts_ls', 'vue_ls' })
 
 if vim.fn.executable("superhtml") == 1 then
     require 'lspconfig'.superhtml.setup {}
